4ce4dc31b23e0a9189a2065dafd89fea724650a0
douglas <douglasc.dev@gmail.com>
2025-11-20 15:49:22 -0300

    remove hardcoded url, add as env var

--- send_push.py
+++ send_push.py
@@ -9,12 +9,13 @@ from requests import post
 
 def main():
     secret = getenv("WEBHOOK_SECRET", "")
+    url = getenv("WEBHOOK_URL", "")
     content = ""
     hash_object = hmac.new(
         secret.encode("utf-8"), msg=content.encode("utf-8"), digestmod=sha256
     )
     req = post(
-        "https://unspaced-aimee-flauntingly.ngrok-free.dev",
+        url,
         headers={
             "x-github-event": "push",
             "X-Hub-Signature-256": f"sha256={hash_object.hexdigest()}",
